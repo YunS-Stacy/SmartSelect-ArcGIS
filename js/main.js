41ef8a9a1fd589423bb02dbd3a5d842cf404a3fc
Angela <shiyun_stacy@yahoo.com>
2017-06-19 15:19:08 -0400

    filter parcel through slider

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-// Set up the map and basic layer (parcel)
 require([
   'esri/Map',
-
+  // basic setting
   'esri/views/MapView',
   'esri/views/SceneView',
+  'esri/Camera',
   // sync views
   'esri/core/watchUtils',
   // layer type
@@ -11,7 +11,7 @@ require([
   // data visualization
   'esri/renderers/SimpleRenderer',
   'esri/renderers/ClassBreaksRenderer',
-  "esri/renderers/smartMapping/creators/size",
+  'esri/renderers/smartMapping/creators/size',
   'esri/renderers/smartMapping/statistics/classBreaks',
   'esri/renderers/smartMapping/statistics/histogram',
   'esri/symbols/PolygonSymbol3D',
@@ -33,7 +33,7 @@ require([
   'dojo/domReady!'
 ], function(
   // basemap
-  Map, MapView, SceneView, watchUtils,
+  Map, MapView, SceneView, Camera, watchUtils,
   FeatureLayer,
   SimpleRenderer, ClassBreaksRenderer, sizeRendererCreator, classBreaks, histogram,
   PolygonSymbol3D, ExtrudeSymbol3DLayer, SimpleFillSymbol,
@@ -119,14 +119,6 @@ require([
   // define renderer
   var parcelRenderer = new ClassBreaksRenderer({
     field: 'refprice',
-    defaultSymbol: new SimpleFillSymbol({
-      color: 'grey',
-      outline: {
-        width: 0.3,
-        color: 'grey'
-      }
-    }),
-    defaultLabel: 'Missing Data',
   });
   // import hosted layer and the fields
   var parcelLyr = new FeatureLayer({
@@ -134,7 +126,7 @@ require([
     outFields: ['location', 'refprice'],
     popupTemplate: {
       title: 'PARCEL INFO',
-      content: "<p>Address: {location}<br></br>Reference Price: ${refprice}<br></br><button class='mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored'><i class='material-icons'>add</i></button></p>",
+      content: "<p>Address: {location}<br></br>Reference Price: ${refprice}</p>",
       fieldInfos: [{
         fieldName: 'refprice',
         label: 'Reference Price',
@@ -157,14 +149,6 @@ require([
     },
   }),
   cl2 = new SimpleFillSymbol({
-    color: 'rgba(34, 94, 168, 0.7)',
-    style: 'solid',
-    outline: {
-      width: 0.3,
-      color: 'rgba(34, 94, 168, 0.7)'
-    },
-  }),
-  cl3 = new SimpleFillSymbol({
     color: 'rgba(29, 145, 192, 0.7)',
     style: 'solid',
     outline: {
@@ -172,23 +156,15 @@ require([
       color: 'rgba(29, 145, 192, 0.7)'
     },
   }),
-  cl4 = new SimpleFillSymbol({
-    color: 'rgba(65, 182, 196, 0.7)',
-    style: 'solid',
-    outline: {
-      width: 0.3,
-      color: 'rgba(65, 182, 196, 0.7)'
-    },
-  }),
-  cl5 = new SimpleFillSymbol({
-    color: 'rgba(12, 44, 132, 0.7)',
+  cl3 = new SimpleFillSymbol({
+    color: 'rgba(127, 205, 187, 0.7)',
     style: 'solid',
     outline: {
       width: 0.3,
       color: 'rgba(127, 205, 187, 0.7)'
     },
   }),
-  cl6 = new SimpleFillSymbol({
+  cl4 = new SimpleFillSymbol({
     color: 'rgba(254, 190, 18, 0.7)',
     style: 'solid',
     outline: {
@@ -196,39 +172,15 @@ require([
       color: 'rgba(254, 190, 18, 0.7)'
     },
   }),
-  cl7 = new SimpleFillSymbol({
-    color: 'rgba(238, 131, 110, 0.7)',
-    style: 'solid',
-    outline: {
-      width: 0.3,
-      color: 'rgba(238, 131, 110, 0.7)'
-    },
-  }),
-  cl8 = new SimpleFillSymbol({
-    color: 'rgba(232, 92, 65, 0.7)',
-    style: 'solid',
-    outline: {
-      width: 0.3,
-      color: 'rgba(232, 92, 65, 0.7)'
-    },
-  }),
-  cl9 = new SimpleFillSymbol({
+  cl5 = new SimpleFillSymbol({
     color: 'rgba(219, 58, 27, 0.7)',
     style: 'solid',
     outline: {
       width: 0.3,
       color: 'rgba(219, 58, 27, 0.7)'
     },
-  }),
-  cl10 = new SimpleFillSymbol({
-    color: 'rgba(170, 45, 23, 0.7)',
-    style: 'solid',
-    outline: {
-      width: 0.3,
-      color: 'rgba(170, 45, 23, 0.7)'
-    },
   });
-  var arrCl = [cl1, cl2, cl3, cl4, cl5, cl6, cl7, cl8, cl9, cl10]
+  var arrCl = [cl1, cl2, cl3, cl4, cl5]
   // classify the data by quantile
   classBreaks({
     layer: parcelLyr,
@@ -236,7 +188,7 @@ require([
     classificationMethod: 'quantile',
     minValue: 69100,
     maxValue: 600000,
-    numClasses: 10,
+    numClasses: 5,
   }).then(function(res){
     // class break infos that may be passed to the
     // constructor of a ClassBreaksRenderer
@@ -260,8 +212,14 @@ require([
   var sceneView = new SceneView({
     container: 'sceneviewDiv',
     map: sceneMap,
-    scale: 5000,
-    center: [-75.16256149898815, 39.982102298576656],
+    camera: new Camera({
+      tilt: 30,
+      position: {
+        latitude: 39.982102298576656,
+        longitude: -75.16256149898815,
+        z: 500
+      }
+    }),
   });
   sceneView.then(function() {
     sceneView.ui.remove('attribution')
@@ -290,35 +248,40 @@ require([
     // manually construct to control the order
     mapView.ui.empty('top-left');
     // add the ui components
-    console.log('ready');
     // configure slider
     // configure params for size renderer generator
     var sizeParams = {
       layer: parcelLyr,
       basemap: map.basemap,
-      field: "refprice",
+      field: 'refprice',
       legendOptions: {
-        title: "Query Slider"
+        title: 'Query Slider'
       },
       minValue: 69100
     };
     // initialize slider params
     var sliderParams = {
-      container: "slider"
+      container: 'slider',
+      visualVariable: {
+        type: 'size',
+        field: 'refprice',
+        minSize: '1px',
+        maxSize: '1px',
+        minDataValue: 69100,
+        maxDataValue: 600000,
+      },
     };
 
     //use sizeRendererCreator to configure slider stats and histogram
     sizeRendererCreator.createContinuousRenderer(sizeParams)
     .then(function(res) {
       sliderParams.statistics = res.statistics;
-      sliderParams.visualVariable = lang.clone(res.visualVariables[0]);
       return histogram({
         layer: parcelLyr,
         field: sizeParams.field,
         minValue: sizeParams.minValue
       });
-    })
-    .then(function(histogram) {
+    }).then(function(histogram) {
       // when the promise resolves, set the histogram in the slider parameters
       sliderParams.histogram = histogram;
       // construct new sizeslider
@@ -326,160 +289,158 @@ require([
       mapView.ui.add([
         {
           component: new Search({ view: mapView }),
-          position: "top-left",
+          position: 'top-left',
           index: 0,
         }, {
           component: new Locate({ view: mapView }),
-          position: "top-left",
+          position: 'top-left',
           index: 1,
         }, {
           component: new Zoom({ view: mapView }),
-          position: "top-left",
+          position: 'top-left',
           index: 2,
         }, {
           component: new Compass({ view: mapView }),
-          position: "top-left",
+          position: 'top-left',
           index: 3,
         }, {
           component: new Legend({
             view: mapView,
             layerInfos: [{
+              // vacant parcel layer
+              layer: map.layers.getItemAt(1),
+              title: 'Vacant Parcel'
+            }, {
               // parcel layer
               layer: map.layers.getItemAt(0),
               title: 'Price Range'
-            }],
-          }),
-          position: "bottom-left",
-          index: 0,
-        }, {
-          component: sizeSlider,
-          position: "bottom-left",
-          index: 1,
-        }, {
-          component: new ScaleBar({
-            view: mapView,
-            unit: 'dual'
-          }),
-          position: "bottom-left",
-          index: 2,
-        },
-      ]);
-      mapView.ui.add("sliderDiv", "bottom-left");
-      // when the user slides the handle(s), update the renderer
-      // with the updated color visual variable object
-
-      // on(sizeSlider, "handle-value-change", function() {
-      //   var renderer = parcelLyr.renderer.clone();
-      //   renderer.visualVariables = [lang.clone(sizeSlider.visualVariable)];
-      //   parcelLyr.renderer = renderer;
-      // });
+            },
+          ],
+        }),
+        position: 'bottom-left',
+        index: 0,
+      }, {
+        component: sizeSlider,
+        position: 'bottom-left',
+        index: 1,
+      }, {
+        component: new ScaleBar({
+          view: mapView,
+          unit: 'dual'
+        }),
+        position: 'bottom-left',
+        index: 2,
+      },
+    ]);
+    // when slider range is changed, update the graphics shown in featurelayer (feature service directly)
+    on(sizeSlider, "handle-value-change', function() {
+      parcelLyr.definitionExpression = 'refprice BETWEEN ' + sizeSlider.values[0] + ' AND ' + sizeSlider.values[1];
     });
+  });
+});
 
-
+mapView.on('click', function(evt){
+  var screenPoint = {
+    x: evt.x,
+    y: evt.y
+  };
+  mapView.popup.watch('visible', function(visible) {
+    console.log('popup visible: ', visible);
   });
 
-  mapView.on('click', function(evt){
-    var screenPoint = {
-      x: evt.x,
-      y: evt.y
-    };
-    mapView.popup.watch('visible', function(visible) {
-      console.log('popup visible: ', visible);
-    });
+});
 
-  });
+/**
+* utility method that synchronizes the viewpoint of a view to other views
+*/
+var synchronizeView = function(view, others) {
+  others = Array.isArray(others) ? others : [others];
 
-  /**
-  * utility method that synchronizes the viewpoint of a view to other views
-  */
-  var synchronizeView = function(view, others) {
-    others = Array.isArray(others) ? others : [others];
+  var viewpointWatchHandle;
+  var viewStationaryHandle;
+  var otherInteractHandlers;
+  var scheduleId;
 
-    var viewpointWatchHandle;
-    var viewStationaryHandle;
-    var otherInteractHandlers;
-    var scheduleId;
+  var clear = function() {
+    if (otherInteractHandlers) {
+      otherInteractHandlers.forEach(function(handle) {
+        handle.remove();
+      });
+    }
+    viewpointWatchHandle && viewpointWatchHandle.remove();
+    viewStationaryHandle && viewStationaryHandle.remove();
+    scheduleId && clearTimeout(scheduleId);
+    otherInteractHandlers = viewpointWatchHandle =
+    viewStationaryHandle = scheduleId = null;
+  };
 
-    var clear = function() {
-      if (otherInteractHandlers) {
-        otherInteractHandlers.forEach(function(handle) {
-          handle.remove();
+  var interactWatcher = view.watch('interacting,animation',
+  function(newValue) {
+    if (!newValue) {
+      return;
+    }
+    if (viewpointWatchHandle || scheduleId) {
+      return;
+    }
+
+    // start updating the other views at the next frame
+    scheduleId = setTimeout(function() {
+      scheduleId = null;
+      viewpointWatchHandle = view.watch('viewpoint',
+      function(newValue) {
+        others.forEach(function(otherView) {
+          otherView.viewpoint = newValue;
         });
-      }
-      viewpointWatchHandle && viewpointWatchHandle.remove();
-      viewStationaryHandle && viewStationaryHandle.remove();
-      scheduleId && clearTimeout(scheduleId);
-      otherInteractHandlers = viewpointWatchHandle =
-      viewStationaryHandle = scheduleId = null;
-    };
+      });
+    }, 0);
 
-    var interactWatcher = view.watch('interacting,animation',
-    function(newValue) {
-      if (!newValue) {
-        return;
-      }
-      if (viewpointWatchHandle || scheduleId) {
-        return;
-      }
-
-      // start updating the other views at the next frame
-      scheduleId = setTimeout(function() {
-        scheduleId = null;
-        viewpointWatchHandle = view.watch('viewpoint',
-        function(newValue) {
-          others.forEach(function(otherView) {
-            otherView.viewpoint = newValue;
-          });
-        });
-      }, 0);
-
-      // stop as soon as another view starts interacting, like if the user starts panning
-      otherInteractHandlers = others.map(function(otherView) {
-        return watchUtils.watch(otherView,
-          'interacting,animation',
-          function(
-            value) {
-              if (value) {
-                clear();
-              }
-            });
-          });
-
-          // or stop when the view is stationary again
-          viewStationaryHandle = watchUtils.whenTrue(view,
-            'stationary', clear);
-          });
-
-          return {
-            remove: function() {
-              this.remove = function() {};
+    // stop as soon as another view starts interacting, like if the user starts panning
+    otherInteractHandlers = others.map(function(otherView) {
+      return watchUtils.watch(otherView,
+        'interacting,animation',
+        function(
+          value) {
+            if (value) {
               clear();
-              interactWatcher.remove();
             }
-          }
-        };
-
-        /**
-        * utility method that synchronizes the viewpoints of multiple views
-        */
-        var synchronizeViews = function(views) {
-          var handles = views.map(function(view, idx, views) {
-            var others = views.concat();
-            others.splice(idx, 1);
-            return synchronizeView(view, others);
           });
+        });
 
-          return {
-            remove: function() {
-              this.remove = function() {};
-              handles.forEach(function(h) {
-                h.remove();
-              });
-              handles = null;
-            }
+        // or stop when the view is stationary again
+        viewStationaryHandle = watchUtils.whenTrue(view,
+          'stationary', clear);
+        });
+
+        return {
+          remove: function() {
+            this.remove = function() {};
+            clear();
+            interactWatcher.remove();
           }
         }
+      };
 
-        // bind the views
-        synchronizeViews([sceneView, mapView]);
-      });
+      /**
+      * utility method that synchronizes the viewpoints of multiple views
+      */
+      var synchronizeViews = function(views) {
+        var handles = views.map(function(view, idx, views) {
+          var others = views.concat();
+          others.splice(idx, 1);
+          return synchronizeView(view, others);
+        });
+
+        return {
+          remove: function() {
+            this.remove = function() {};
+            handles.forEach(function(h) {
+              h.remove();
+            });
+            handles = null;
+          }
+        }
+      }
+
+      // bind the views
+      synchronizeViews([sceneView, mapView]);
+    });
